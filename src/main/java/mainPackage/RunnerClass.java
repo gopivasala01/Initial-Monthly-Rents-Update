933935b428dbaaada2c72e5483dd796ee597bcce
gopivasala01 <49265100+gopivasala01@users.noreply.github.com>
2023-07-27 02:39:01 -0400

    Update RunnerClass.java

--- src/main/java/mainPackage/RunnerClass.java
+++ src/main/java/mainPackage/RunnerClass.java
@@ -43,7 +43,7 @@ public class RunnerClass
 		DataBase.getBuildingsList(AppConfig.pendingLeasesQuery);
 		while(w<3)
 		{
-		for(int i=0;i<10;i++)  //pendingLeases.length
+		for(int i=0;i<pendingLeases;i++)  //pendingLeases.length
 		{
 			  System.out.println("-------------------------------------------------------------");
 			  company = pendingLeases[i][0];
