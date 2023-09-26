f0d8c9fb00834998df755d1d06a506cf1f5d10aa
gopivasala01 <gopi.v@beetlerim.com>
2023-09-26 01:02:46 -0400

    Update

--- src/main/java/mainPackage/AppConfig.java
+++ src/main/java/mainPackage/AppConfig.java
@@ -9,7 +9,7 @@ public class AppConfig
 	   public static String password ="KRm#V39fecMDGg#";
 	   
 	   public static String excelFileLocation = "E:\\Automation\\Initial Rents Update";
-	   public static String downloadFilePath = "C:\\SantoshMurthyP\\Initial Rents Update";
+	   public static String downloadFilePath = "C:\\SantoshMurthyP\\Initial Rents Update - Branches\\Savannah and SouthCarolina";
 	   //Mail credentials
 	   public static String fromEmail = "bireports@beetlerim.com";
 	   public static String fromEmailPassword = "Welcome@123";
@@ -26,8 +26,15 @@ public class AppConfig
 	   
 	  // public static String leaseFetchQuery  = "Select Company, Building,leaseName from Automation.InitialRentsUpdate where Status ='Pending' and Company ='Georgia'";
 	   
-	   public static String pendingLeasesQuery = "Select Company, building, leasename from Automation.InitialRentsUpdate \r\n"
-	   		+ "where Status='Pending' and LeaseStatus Not in ('Dead Application','Terminated')  and company in ('South Carolina', 'Savannah')";
+	   public static String pendingLeasesQuery = "\r\n"
+	   		+ "WITH CTE AS\r\n"
+	   		+ "(\r\n"
+	   		+ "Select *, CAST(REPLACE(MonthlyRent,',','') as Decimal(18,2)) \r\n"
+	   		+ "- CAST(REPLACE(MonthlyRentFromPW,',','') as Decimal(18,2)) as Differene  from  [automation].[initialrentsupdate] \r\n"
+	   		+ "where  MonthlyRent <> '' and MonthlyRent is not null and MonthlyRentFromPW is not null and MonthlyRentFromPW <>'' and MonthlyRentFromPW <>'Error' and\r\n"
+	   		+ "CAST(REPLACE(MonthlyRent,',','') as Decimal(18,2)) >0.00 and CAST(REPLACE(MonthlyRentFromPW,',','') as Decimal(18,2))>0.00\r\n"
+	   		+ ")\r\n"
+	   		+ "Select Company, Building, LeaseName, MonthlyRent, MonthlyRentFromPW from CTE where Differene>100 and Status='Completed' and LeaseStatus Not in ('Dead Application','Terminated')  and company in (''South Carolina', 'Savannah'')";
 	   
 	   public static String failedLeasesQuery = "Select Company, Building,leaseName from Automation.InitialRentsUpdate where Status ='Failed'  and (Notes ='Building Not Found' or  Notes = ',Unable to Click Lease Onwer Name') and company in ('South Carolina', 'Savannah')";	   
 	   public static String getLeasesWithStatusforCurrentDay = "Select Company, Building,ThirdPartyUnitID, Leaseidnumber, LeaseName,LeaseStatus,leaseExecutionDate, StartDate, EndDate, MonthlyRent, MonthlyRentFromPW, PetRent, PetRentFromPW,Status, Notes from Automation.InitialRentsUpdate ";//where Format(convert(datetime, CompletedDate, 101),'dd MM yyyy') = format(getdate(),'dd MM yyyy') ";//and company in ('Florida','North Carolina')";
